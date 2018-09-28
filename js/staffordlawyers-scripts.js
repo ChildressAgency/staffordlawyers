4aac5d3bfc363837c7ca01574ed4391bbd16ce6f
childressdev <jcampbell@childressagency.com>
2018-09-28 13:39:46 -0400

    library demo styled

--- js/staffordlawyers-scripts.js
+++ js/staffordlawyers-scripts.js
@@ -1,3 +1,23 @@
 jQuery(document).ready(function($){
+  $('.slider-panel').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    infinite: false,
+    prevArrow: '<i class="fas fa-angle-left"></i>',
+    nextArrow: '<i class="fas fa-angle-right"></i>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
+  });
 
+  //$('#library').on('shown.bs.collapse', function(){
+  //  $('.slider-panel').get(0).slick.setPosition();
+  //});
 });
