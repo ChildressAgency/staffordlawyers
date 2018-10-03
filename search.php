aa5d03af660f30c09de85b18c600fc2eefbb43f0
childressdev <jcampbell@childressagency.com>
2018-10-03 16:30:04 -0400

    added wp_pagenavi

--- search.php
+++ search.php
@@ -1,16 +1,21 @@
 <?php get_header(); ?>
   <main id="main">
     <div class="container">
-      <h2>Blog Posts</h2>
-      <?php
+      <section id="intro-search">
+        <?php get_search_form(); ?>
+      </section>
+
+      <h2>Showing results for <?php echo get_search_query(); ?></h2>
+      <?php 
         if(have_posts()){
           while(have_posts()){
             the_post();
+
             get_template_part('partials/post-summary', 'loop');
           }
         }
         else{
-          echo '<p>Sorry, there is nothing to display.</p>';
+          echo '<p>Sorry, no results were found.</p>';
         } wp_pagenavi();
       ?>
     </div>
