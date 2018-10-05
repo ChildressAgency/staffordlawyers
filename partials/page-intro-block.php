36675025660a35b755f11a92892b9919c8658f5d
childressdev <jcampbell@childressagency.com>
2018-10-05 08:51:56 -0400

    intro from base page for cpts

--- partials/page-intro-block.php
+++ partials/page-intro-block.php
@@ -1,6 +1,24 @@
-<?php if(get_field('page_intro_title') || get_field('page_intro')): ?>
+<?php 
+if(is_home()){
+  $intro_page = get_page_by_path('blog');
+}
+else if(is_single('practice_areas')){
+  $intro_page = get_page_by_path('practice-areas');
+}
+else if(is_single('library')){
+  $intro_page = get_page_by_path('library');
+}
+
+if($intro_page){
+  $intro_page_id = $intro_page->ID;
+}
+else{
+  $intro_page_id = get_the_ID();
+}
+
+if(get_field('page_intro_title', $intro_page_id) || get_field('page_intro', $intro_page_id)): ?>
   <article class="brief">
-    <h2 class="underline-header"><?php the_field('page_intro_title'); ?></h2>
-    <?php the_field('page_intro'); ?>
+    <h2 class="underline-header"><?php the_field('page_intro_title', $intro_page_id); ?></h2>
+    <?php the_field('page_intro', $intro_page_id); ?>
   </article>
 <?php endif; ?>
