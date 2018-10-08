3a912c59859e72de96c01052a113ef5148f53300
childressdev <jcampbell@childressagency.com>
2018-10-08 09:54:21 -0400

    added

---
+++ page-testimonials.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+  <main id="main">
+    <div class="container">
+      <?php get_template_part('partials/page-intro', 'block'); ?>
+    </div>
+  </main>
+
+  <?php if(have_rows('testimonials')): ?>
+    <section id="client-reviews" style="background-image:url(<?php echo get_stylesheet_directory_uri(); ?>/images/up-columns.jpg); background-position:bottom center;">
+      <div class="container">
+        <?php while(have_rows('testimonials')): the_row(); ?>
+          <div class="client-review">
+            <?php the_sub_field('testimonial'); ?>
+            <cite>&mdash;<?php the_sub_field('testimonial_author'); ?></cite>
+          </div>
+        <?php endwhile; ?>
+      </div>
+    </section>
+  <?php endwhile; endif; ?>
+<?php get_footer(); ?>
