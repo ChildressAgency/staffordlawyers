e0b27a2ab075fe9349c251d117c57d14f2c219f4
childressdev <jcampbell@childressagency.com>
2018-10-08 09:57:41 -0400

    show cat list instead of just 1

--- partials/post-summary-block.php
+++ partials/post-summary-block.php
@@ -6,12 +6,15 @@
       $adjacent_post_date = get_the_date('F j, Y', $adjacent_post->ID);
       $adjacent_post_author_archive_link = get_author_posts_url($adjacent_post->post_author);
       $adjacent_post_author_name = get_the_author_meta('display_name', $adjacent_post->post_author);
+
       $adjacent_post_cats = get_the_category($adjacent_post->ID);
-      $adjacent_post_cat_name = $cur_cats[0]->name;
-      $adjacent_post_cat_id = $cur_cats[0]->ID;
-      $adjacent_post_cat_archive_link = get_category_link($adjacent_post_cat_id);
+      $adjacent_post_cat_links = [];
+      foreach($adjacent_post_cats as $adjacent_post_cat){
+        $adjacent_post_cat_links[] = '<a href="' . get_category_link($adjacent_post_cat) . '">' . $adjacent_post_cat->name . '</a>';
+      }
+      $adjacent_post_cats_list = implode(', ', $adjacent_post_cat_links);
     ?>
-    <p>Posted on <?php echo $adjacent_post_date; ?> by <a href="<?php echo $adjacent_post_author_archive_link; ?>"><?php echo $adjacent_post_author_name; ?></a> in <a href="<?php echo $adjacent_post_cat_archive_link; ?>"><?php echo $adjacent_post_cat_name; ?></a></p>
+    <p>Posted on <?php echo $adjacent_post_date; ?> by <a href="<?php echo $adjacent_post_author_archive_link; ?>"><?php echo $adjacent_post_author_name; ?></a> in <?php echo $adjacent_post_cats_list; ?></p>
   </header>
   <?php echo $adjacent_post->post_excerpt(); ?>
   <a href="<?php echo get_permalink($adjacent_post->ID); ?>" class="read-article">Read Article.</a>
